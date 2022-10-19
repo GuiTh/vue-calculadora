348e61aaff4d0e0c83fc079d9269e776d5d02798
GuiTh <guilhermetcarlos@hotmail.com>
2022-10-19 13:24:55 -0300

    att

---
+++ src/main/Calculator.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="calculator">
+    <Display />
+    <Button />
+  </div>
+</template>
+
+<script>
+import Display from "../components/Display"
+import Button from "../components/Button"
+export default {
+    components:{Button,Display}
+}
+</script>
+
+<style>
+.calculator{
+    height: 320px;
+    width:235px ;
+    
+}
+</style>
